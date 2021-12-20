68f22c6d8a87a0274413f86cbfb5be0546733125
Amandeep <asuppal91@gmail.com>
2021-12-20 22:57:02 +0530

    more angular material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,9 +6,11 @@ import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from "@angular/material/core";
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatNativeDateModule, MatIconModule, MatDatepickerModule, MatFormFieldModule, MatInputModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatNativeDateModule, MatIconModule, MatDatepickerModule, MatFormFieldModule, MatInputModule]
+    imports: [MatButtonModule, MatCheckboxModule, MatNativeDateModule, MatIconModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatToolbarModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatNativeDateModule, MatIconModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatSidenavModule,  MatToolbarModule]
 })
 export class MaterialModule {}
